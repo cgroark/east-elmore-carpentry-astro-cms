ccda069afe4e897e08fd2607ea24aa6cf05cb8ab
Colin Groark <colin@nexleaf.org>
2026-01-23 12:59:21 +0000

    updated url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  site: 'https://placeholder.netlify.app',
+  site: 'https://east-elmore-carpentry.netlify.app',
   integrations: [react()],
 });
